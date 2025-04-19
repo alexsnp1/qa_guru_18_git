7553cd40080506c7059686b3de57f89db64322b1
Alex <164615604+alexsnp1@users.noreply.github.com>
2025-04-19 17:51:01 +0700

    Update Main.java

--- src/main/java/com/github/alexsnp1/Main.java
+++ src/main/java/com/github/alexsnp1/Main.java
@@ -2,7 +2,7 @@ package com.github.alexsnp1;
 
 public class Main {
 
-private static String msg = "Hello, qa_guru !!!"
+private static String msg = "Hello, qa_guru !"
     public static void main(String[] args) {
         System.out.println(msg);
     }
